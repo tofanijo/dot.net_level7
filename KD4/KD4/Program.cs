491f232c38261b03ec5e749a0d484a477678b407
tofanijo <tofanijo@gmail.com>
2020-01-19 20:23:07 +0200

    KD4

---
+++ KD4/KD4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sukurti struktūrą Studentas, su
+            //laukais ID ir įskaitos rezultatas(bool ).
+            //Sukurti studentų sąrašą
+            //List<Studentas>, pridėti 5 studentus į
+            //sąrašą.Atskiroje funkcijoje išvesti
+            //sąrašą į konsolę.
+
+            List<Studentas> studentuSarasas = new List<Studentas>();
+            studentuSarasas.Add(new Studentas(1, true));
+            studentuSarasas.Add(new Studentas(2, false));
+            studentuSarasas.Add(new Studentas(3, true));
+            studentuSarasas.Add(new Studentas(4, false));
+            studentuSarasas.Add(new Studentas(5, true));
+
+            IsvestiStudentuDuomenis();
+
+            Console.ReadLine();
+
+            void IsvestiStudentuDuomenis()
+            {
+                foreach (var item in studentuSarasas)
+                {
+                    Console.WriteLine($"Studento ID: {item.id}, Iskaitos rezultatas: {item.iskaitosRezultatas}");
+                }
+            }
+        }
+    }
+    }
---
+++ KD4/KD4/Studentas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD4
+{
+    struct Studentas
+    {
+        public int id;
+        public bool iskaitosRezultatas;
+
+
+
+        public Studentas(int _id, bool rezultatas)
+        {
+            id = _id;
+            iskaitosRezultatas = rezultatas;
+
+        }
+    }
+}
